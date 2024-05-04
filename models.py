6fbc7518607251556a6c1fc9fad4bc50b792405b
VartoS <ngaseneger@gmail.com>
2024-05-04 20:16:42 +0500

    Fixed models

--- models.py
+++ models.py
@@ -18,6 +18,12 @@ class UsersOlympiads(Base):
 
 class UsersInfo(Base):
     __tablename__ = 'users_info'
+    
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    tg_id: Mapped[str]
+    grade: Mapped[int]
+    subjects: Mapped[list[str]] = mapped_column(ARRAY(String(256)))
+    levels: Mapped[list[int]] = mapped_column(ARRAY(Integer))
 
 class OlympiadsInfo(Base):
     __tablename__ = 'olympiads_info'
@@ -31,7 +37,12 @@ class OlympiadsInfo(Base):
 class OlympiadsDates(Base):
     __tablename__ = 'olympiads_dates'
 
-    id: Mapped[innt] = mapped_column(primary_key=True, autoincrement=True)
-    olymp_id: Mapped[int] = mapped_column(ForeignKey='olympiads_info.olymp_id')
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    olymp_id: Mapped[int]
     stage_name: Mapped[str]
-    
+    date_from: Mapped[datetime.datetime]
+    date_to: Mapped[datetime.datetime]
+
+    __table_args__ = (
+        ForeignKeyConstraint(['olymp_id'], ['olympiads_info.olymp_id']),
+    )
