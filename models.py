ada8124932514b16c744a83eac71ad6f66fea700
VartoS <ngaseneger@gmail.com>
2024-05-04 19:21:26 +0500

    Models ready

--- models.py
+++ models.py
@@ -11,12 +11,19 @@ class Base(DeclarativeBase):
 class UsersOrm(Base):
     __tablename__ = 'users_olympiads'
 
-    tg_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=False)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    tg_id: Mapped[str] = mapped_column(autoincrement=False)
     followed_olymp: Mapped[int]
 
 
 class UsersInfo(Base):
     __tablename__ = 'users_info'
+    
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    tg_id: Mapped[str]
+    grade: Mapped[int]
+    subjects: Mapped[list[str]]
+    levels: Mapped[list[int]]
 
 class OlympiadsInfo(Base):
     __tablename__ = 'olympiads_info'
@@ -31,7 +38,8 @@ class OlympiadsInfo(Base):
 class OlympiadsDates(Base):
     __tablename__ = 'olympiads_dates'
 
-    id: Mapped[innt] = mapped_column(primary_key=True, autoincrement=True)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     olymp_id: Mapped[int] = mapped_column(ForeignKey='olympiads_info.olymp_id')
     stage_name: Mapped[str]
-    
+    date_from: Mapped[datetime.datetime]
+    date_to: Mapped[datetime.datetime]
