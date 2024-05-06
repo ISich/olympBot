2edba9e947cae3ea6a7d52feba15b50b2e3cbe61
kulikovartem <artemkulikovsprog@gmail.com>
2024-05-06 17:19:13 +0500

    orm done

--- orm.py
+++ orm.py
@@ -1,6 +1,6 @@
 from db import session_factory, sync_engine
-from models import Base, UsersOrm, OlympiadsOrm
-
+from models import Base, UsersOlympiads, UsersInfo, OlympiadsInfo, OlympiadsDates
+from sqlalchemy import or_
 
 class SyncOrm():
     @staticmethod
@@ -44,28 +44,73 @@ class SyncOrm():
     @staticmethod
     def add_user_info(tg_id: str, grade: int, subjects: list[int], levels: list[int]) -> None:
         #добавляет пользователя с этими данными в табличку users_info
-        pass
-
+        with session_factory() as session:
+            try:
+                user = UsersInfo(tg_id=tg_id, grade=grade, subjects=subjects, levels=levels)
+                session.add(user)
+                session.commit()
+            except Exception as e:
+                session.rollback()
+                raise e
 
     @staticmethod
-    def get_olympiads_interesting_for_user(tg_id: str) -> OlympiadsInfo:
-        #Возвращает список олимпиад, по фильтрам, который указал пользователь при регистрации aka предметы, уровень и тд
-        pass
+    def get_olympiads_interesting_for_user(tg_id: str) -> list[OlympiadsInfo]:
+        with session_factory() as session:
+            userInfo = session.query(UsersInfo).filter(UsersInfo.tg_id == tg_id).first()
+            if userInfo:
+                subjects = userInfo.subjects
+                levels = userInfo.levels
+                olymps = session.query(OlympiadsInfo).filter(or_(OlympiadsInfo.subject.in_(subjects),
+                                                                 OlympiadsInfo.level.in_(levels))).all()
+                return olymps
+            else:
+                return []
 
     @staticmethod
     def subscibe_on_all_olympiads(tg_id: str) -> None:
         #Должен добавить для конкретного user'a по его tg_id в табличку users_olumpiads все те олимпиады, которые подходят ему по фильтрам из его users_info
-        pass
+        with session_factory() as session:
+            user_info = session.query(UsersInfo).filter(UsersInfo.tg_id == tg_id).first()
+            if user_info:
+                subjects = user_info.subjects
+                levels = user_info.levels
+                olympiads = session.query(OlympiadsInfo).filter(
+                    OlympiadsInfo.subject.in_(subjects),
+                    OlympiadsInfo.level.in_(levels)
+                ).all()
+                for olympiad in olympiads:
+                    user_info.followed_olymp.append(olympiad[0])
+                session.commit()
+            else:
+                print("Пользователь с таким tg_id не найден.")
 
     @staticmethod
     def get_olympiad_by_name(olympiad_name: str) -> OlympiadsInfo:
         # По имени олимпиады возвращает ее можельку, которую мы на постпроцессинге преобразуем в сообщение
-        pass
+        with session_factory() as session:
+            olymp = session.query(OlympiadsInfo).filter(OlympiadsInfo.name == olympiad_name).first()
+            if olymp:
+                return olymp
+            print("Олимпиада с таким именем не найдена")
 
+    @staticmethod
     def subscribe_on_olympiads_by_names(tg_id:str, olympiads_names: list[str]) -> None:
         # По массиву из имен олимпиал подписывает пользователя на олимпиады по имени и фильтрам, те добовляет в таблицу users_olumpiads соответствующие записи
-        pass
+        with session_factory() as session:
+            userinfo = session.query(UsersInfo).filter(tg_id=tg_id).first()
+            levels = userinfo.levels
+            subjects = userinfo.subjects
+            olymps = session.query(OlympiadsInfo).filter(OlympiadsInfo.subject.in_(subjects),
+                                                         OlympiadsInfo.level.in_(levels)).all()
+            for olymp in olymps:
+                userinfo.followed_olymp.append(olymp[0])
+            session.commit()
 
+    @staticmethod
     def get_all_users_subscriptions() -> list[UsersOlympiads]:
         #Просто возвращает все такие модельки из базы
-        pass
+        with session_factory() as session:
+            all_users_subs = session.query(UsersInfo).all()
+            return all_users_subs
+
+
