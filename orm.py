864dedeac3b9ad76939a3a76e6c6ca3bb9db32e4
kulikovartem <artemkulikovsprog@gmail.com>
2024-05-06 21:02:09 +0500

    Delete not needed funcs

--- orm.py
+++ orm.py
@@ -2,6 +2,7 @@ from db import session_factory, sync_engine
 from models import Base, UsersOlympiads, UsersInfo, OlympiadsInfo, OlympiadsDates
 from sqlalchemy import or_
 
+
 class SyncOrm():
     @staticmethod
     def create_tables() -> None:
@@ -10,36 +11,6 @@ class SyncOrm():
         Base.metadata.create_all(sync_engine)
         sync_engine.echo = True
 
-    @staticmethod
-    def select_user(tg_id: int) -> list[int]:
-        with session_factory() as session:
-            user = session.query(UsersOrm).filter(UsersOrm.tg_id == tg_id).first()
-            return user.followed_olymps
-
-    @staticmethod
-    def add_user(tg_id: int, olymps : list[int] = []) -> None:
-        with session_factory() as session:
-            try:
-                new_user = UsersOrm(tg_id=tg_id, followed_olymps=olymps)
-                session.add(new_user)
-                session.commit()
-            except Exception as e:
-                session.rollback()
-                raise e
-
-    @staticmethod
-    def delete_user(tg_id: int) -> None:
-        with session_factory() as session:
-            try:
-                user = session.query(UsersOrm).filter(UsersOrm.tg_id == tg_id).first()
-                if user:
-                    session.delete(user)
-                    session.commit()
-                else:
-                    print(f"Пользователь с tg_id={tg_id} не найден.")
-            except Exception as e:
-                session.rollback()
-                raise e
 
     @staticmethod
     def add_user_info(tg_id: str, grade: int, subjects: list[int], levels: list[int]) -> None:
